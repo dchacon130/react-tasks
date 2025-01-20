5ef13ddf1609b5775cf4af78a56def11e6358305
Diego Chacon <dchacon130@gmail.com>
2025-01-20 12:19:46 -0500

    Diseno de los componentes new task y list grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { FaRegCheckCircle,FaRegTrashAlt } from "react-icons/fa";
+
+function AddTask(){
+  return(
+    <div className='formDiv'>
+      <form>
+        <div>
+        <label className='lblTitleTask'>Title</label><br/>
+        <input type='text' className='txtTitleTask' id='txtTitleTask' placeholder='Title' required/>
+        </div>
+        <div>
+        <label className='lblCommentTask'>Comment</label><br/>
+        <textarea id='txtCommnetTask' className='txtCommnetTask' rows="5" cols="55"></textarea>
+        </div>
+        <div>
+        <label className='lblStatusTask'>Status</label><br/>
+        <select className='slStatusTask' id='slStatusTask'>
+          <option value={""}>Select</option>
+          <option value={"completed"}>Completed</option>
+          <option value={"in progress"}>In progress</option>
+          <option value={"pending"}>Pending</option>
+        </select>
+        <button className='buttonAdd' type='button'>Add Task</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+function GridTask(){
+  return(
+    <>
+    <h1>Task list</h1>
+    <table className='tableList'>
+      <tr>
+        <th>Title</th>
+        <th>Comment</th>
+        <th>Status</th>
+        <th></th>
+      </tr>
+      <tr>
+        <td>Task001</td>
+        <td>asdf asdf asf</td>
+        <td>In progress</td>
+        <td>
+          <li className='doneTask'><FaRegCheckCircle /></li>
+          <li className='deleteTask'><FaRegTrashAlt /></li>
+        </td>
+      </tr>
+    </table>
+    </>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Task list</h1>
+      <AddTask/>
+      <GridTask/>
     </>
   )
 }
